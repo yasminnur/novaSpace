3657a253c8668f4b0d418fdada78a02f4f9af6c6
yasminnur <yasminnurazmina@gmail.com>
2025-07-01 16:04:35 +0700

    Landing Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,40 +1,145 @@
+"use client";
 import Image from "next/image";
-
+import Link from "next/link";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
 export default function Home() {
+  const pathname = usePathname();
+  const [selected, setSelected] = useState("alpha");
   return (
     <>
-      {/* <div className="bg-primary px-7 py-12 w-screen"> */}
+      <div id="about" className="flex flex-col gap-y-[30px]">
         <section
-          style={{ backgroundImage: "url(/assets/img/img-section1.svg)", backgroundPosition: "center", backgroundRepeat:"no-repeat", backgroundSize:"cover" }}
-          className="rounded-[60px] flex justify-center items-center md:rounded-[45px] min-h-screen overflow-x-hidden"
-      >
-        <div className="flex flex-col justify-center items-center text-white font-normal uppercase leading-none">
-          <h1 className="text-[120px] font-[var(--font-inter)]">Work<span className="italic font-[var(--font-serif)]"> Smarter,</span></h1>
-          <h1 className="text-[120px] italic"><span className="italic font-[Instrument Serif]">InNovate</span> faster</h1>
-          <p className="normal-case mt-10 text-lg">Where innovation meets collaboration in a futuristic space.</p>
-        </div>
+          style={{
+            backgroundImage: "url(/assets/img/img-section1.svg)",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+          className="rounded-[60px] flex flex-col justify-center items-center md:rounded-[45px] min-h-screen h-fit overflow-x-hidden py-[283px] px-[128px]"
+        >
+          <div className="flex flex-col justify-center items-center text-white font-normal uppercase leading-none">
+            <h1 className="text-[120px] font-[var(--font-inter)]">
+              Work
+              <span className="italic font-[var(--font-serif)]">
+                {" "}
+                Smarter,{" "}
+              </span>
+            </h1>
+            <h1 className="text-[120px]">
+              <span className="italic font-[Instrument Serif]">InNovate</span>{" "}
+              faster
+            </h1>
+            <p className="normal-case mt-10 text-lg">
+              Where innovation meets collaboration in a futuristic space.
+            </p>
+          </div>
+          <div className="flex justify-center items-center text-white font-normal w-fill gap-x-[171px] mt-[250px]">
+            <h1 className="basis-1/3 text-[70px] italic leading-none">
+              Where Visionaries Create the Future
+            </h1>
+            <p className="basis-2/3 text-2xl leading-loose">
+              Welcome to NovaSpace, a futuristic coworking space designed for
+              creators, innovators, and dreamers. Our space blends cutting-edge
+              design with a collaborative atmosphere, offering a seamless fusion
+              of technology,comfort, and inspiration. Whether you're a startup,
+              freelancer, or remote team, NovaSpace provides the perfect
+              environment to ignite ideas and shape the future.
+            </p>
+          </div>
         </section>
         <section
-          style={{ backgroundImage: "url(/assets/img/img-section1.svg)", backgroundPosition: "center", backgroundRepeat:"no-repeat", backgroundSize:"cover" }}
-          className="rounded-[60px] flex justify-center items-center md:rounded-[45px] min-h-screen overflow-x-hidden"
-      >
-        <div className="flex flex-col justify-center items-center text-white font-normal uppercase leading-none">
-          <h1 className="text-[120px] font-[var(--font-inter)]">Work<span className="italic font-[var(--font-serif)]"> Smarter,</span></h1>
-          <h1 className="text-[120px] italic"><span className="italic font-[Instrument Serif]">InNovate</span> faster</h1>
-          <p className="normal-case mt-10 text-lg">Where innovation meets collaboration in a futuristic space.</p>
-        </div>
+          id="location"
+          className="py-[100px] px-10 rounded-t-[60px] bg-white "
+        >
+          <h1 className="text-center font-normal text-[45px] text-[#191919]">Location</h1>
+          <div className="nav-location mt-[50px] text-[28px]">
+            <ul className="flex gap-x-5">
+              <li
+                className={`flex-1 text-center rounded-2xl text-white w-full py-5`}
+              >
+                <button
+                  onClick={() => setSelected("alpha")}
+                  className={`flex-1 text-center rounded-2xl w-full py-5 ${
+                    selected === "alpha" ? "bg-[#191919] text-white font-medium" : "bg-[#F8F8F8] text-[#191919] font-normal"
+                  }`}
+                >
+                  Alpha District
+                </button>
+              </li>
+              <li
+                className={`flex-1 text-center rounded-2xl text-white w-full py-5`}
+              >
+                <button
+                  onClick={() => setSelected("beta")}
+                  className={`flex-1 text-center rounded-2xl w-full py-5 ${
+                    selected === "beta" ? "bg-[#191919] text-white font-medium" : "bg-[#F8F8F8] text-[#191919] font-normal"
+                  }`}
+                >
+                  Beta District
+                </button>
+              </li>
+              <li
+                className={`flex-1 text-center rounded-2xl text-white w-full py-5`}
+              >
+                <button
+                  onClick={() => setSelected("omega")}
+                  className={`flex-1 text-center rounded-2xl w-full py-5 ${
+                    selected === "omega" ? "bg-[#191919] text-white font-medium" : "bg-[#F8F8F8] text-[#191919] font-normal"
+                  }`}
+                >
+                  Omega District
+                </button>
+              </li>
+            </ul>
+          </div>
+
+          <div className="relative h-[581px] w-full mt-[30px] rounded-[60px] overflow-hidden">
+            {selected === "alpha" && (
+              <>
+                 <Image
+                  src={"/assets/img/img-section1.svg"}
+                  fill
+                  className="absolute inset-0 object-cover w-full h-full"
+                  ></Image>
+                <div className="desc-location text-base text-start font-normal text-[#191919] absolute z-20 left-10 top-10 p-[30px] bg-[#F8F8F8] h-[108px] w-[375px] rounded-3xl ">
+              <p className="truncate">📍 Futurix Tower, 15th Floor</p>
+              <p className="truncate">Jl. Quantum Avenue No. 56, Alpha District</p>
+            </div>
+              </>
+            )}
+            {selected === "beta" && (
+               <>
+               <Image
+                src={"/assets/img/img-section1.svg"}
+                fill
+                className="absolute inset-0 object-cover w-full h-full"
+                ></Image>
+              <div className="desc-location text-base text-start font-normal text-[#191919] absolute z-20 left-10 top-10 p-[30px] bg-[#F8F8F8] h-[108px] w-[375px] rounded-3xl ">
+            <p className="truncate">📍 Futurix Tower, 15th Floor</p>
+            <p className="truncate">Jl. Quantum Avenue No. 56, Beta District</p>
+          </div>
+            </>
+            )}
+            {selected === "omega" && (
+               <>
+               <Image
+                src={"/assets/img/img-section1.svg"}
+                fill
+                className="absolute inset-0 object-cover w-full h-full"
+                ></Image>
+              <div className="desc-location text-base text-start font-normal text-[#191919] absolute z-20 left-10 top-10 p-[30px] bg-[#F8F8F8] h-[108px] w-[375px] rounded-3xl ">
+            <p className="truncate">📍 Futurix Tower, 15th Floor</p>
+            <p className="truncate">Jl. Quantum Avenue No. 56, Omega District</p>
+          </div>
+            </>
+            )}
+          </div>
         </section>
-        <section
-          style={{ backgroundImage: "url(/assets/img/img-section1.svg)", backgroundPosition: "center", backgroundRepeat:"no-repeat", backgroundSize:"cover" }}
-          className="rounded-[60px] flex justify-center items-center md:rounded-[45px] min-h-screen overflow-x-hidden"
-      >
-        <div className="flex flex-col justify-center items-center text-white font-normal uppercase leading-none">
-          <h1 className="text-[120px] font-[var(--font-inter)]">Work<span className="italic font-[var(--font-serif)]"> Smarter,</span></h1>
-          <h1 className="text-[120px] italic"><span className="italic font-[Instrument Serif]">InNovate</span> faster</h1>
-          <p className="normal-case mt-10 text-lg">Where innovation meets collaboration in a futuristic space.</p>
-        </div>
+        <section>
+          
         </section>
-      {/* </div> */}
+      </div>
     </>
   );
 }
--- src/components/navbar/page.jsx
+++ src/components/navbar/page.jsx
@@ -14,21 +14,29 @@ export default function Navbar() {
         </div>
         <div className="text-white text-base">
           <ul className="flex gap-x-5">
-            <li className="relative group overflow-hidden w-[90px] h-[40px] rounded-full hover:bg-white/5 transition-all duration-500 cursor-pointer">
+              <li className="relative group overflow-hidden w-[90px] h-[40px] rounded-full hover:bg-white/5 transition-all duration-500 cursor-pointer">
+                <Link href="#about">
               <p className="absolute inset-0 flex items-center justify-center transition-all duration-500 ease-in-out group-hover:opacity-0 group-hover:-translate-y-full">About</p>
-              <p className="absolute inset-0 flex items-center justify-center transition-all duration-500 ease-in-out opacity-0 group-hover:opacity-100 translate-y-full group-hover:translate-y-0">About</p>
+                  <p className="absolute inset-0 flex items-center justify-center transition-all duration-500 ease-in-out opacity-0 group-hover:opacity-100 translate-y-full group-hover:translate-y-0">About</p>
+                  </Link>
             </li>
-            <li className="relative group overflow-hidden px-4 py-2 w-[90px] h-[40px] rounded-full hover:bg-white/5 transition duration-600 cursor-pointer">
+              <li className="relative group overflow-hidden px-4 py-2 w-[90px] h-[40px] rounded-full hover:bg-white/5 transition duration-600 cursor-pointer">
+              <Link href="#location">
               <p className="absolute inset-0 flex items-center justify-center transition-all duration-500 ease-in-out group-hover:opacity-0 group-hover:-translate-y-full">Location</p>
-              <p className="absolute inset-0 flex items-center justify-center transition-all duration-500 ease-in-out opacity-0 group-hover:opacity-100 translate-y-full group-hover:translate-y-0">Location</p>
+                  <p className="absolute inset-0 flex items-center justify-center transition-all duration-500 ease-in-out opacity-0 group-hover:opacity-100 translate-y-full group-hover:translate-y-0">Location</p>
+                  </Link>
             </li>
-            <li className="relative group overflow-hidden px-4 py-2 w-[90px] h-[40px] rounded-full hover:bg-white/5 transition duration-600 cursor-pointer">
+              <li className="relative group overflow-hidden px-4 py-2 w-[90px] h-[40px] rounded-full hover:bg-white/5 transition duration-600 cursor-pointer">
+              <Link href="#room">
               <p className="absolute inset-0 flex items-center justify-center transition-all duration-500 ease-in-out group-hover:opacity-0 group-hover:-translate-y-full">Room</p>
-              <p className="absolute inset-0 flex items-center justify-center transition-all duration-500 ease-in-out opacity-0 group-hover:opacity-100 translate-y-full group-hover:translate-y-0">Room</p>
+                  <p className="absolute inset-0 flex items-center justify-center transition-all duration-500 ease-in-out opacity-0 group-hover:opacity-100 translate-y-full group-hover:translate-y-0">Room</p>
+                  </Link>
             </li>
-            <li className="relative group overflow-hidden px-4 py-2 w-[90px] h-[40px] rounded-full hover:bg-white/5 transition duration-600 cursor-pointer">
+              <li className="relative group overflow-hidden px-4 py-2 w-[90px] h-[40px] rounded-full hover:bg-white/5 transition duration-600 cursor-pointer">
+              <Link href="#facilites">
               <p className="absolute inset-0 flex items-center justify-center transition-all duration-500 ease-in-out group-hover:opacity-0 group-hover:-translate-y-full">Facilities</p>
-              <p className="absolute inset-0 flex items-center justify-center transition-all duration-500 ease-in-out opacity-0 group-hover:opacity-100 translate-y-full group-hover:translate-y-0">Facilities</p>
+                  <p className="absolute inset-0 flex items-center justify-center transition-all duration-500 ease-in-out opacity-0 group-hover:opacity-100 translate-y-full group-hover:translate-y-0">Facilities</p>
+                  </Link>
             </li>
             <li className="flex justify-center items-center border border-black hover:border-white bg-white hover:bg-transparent hover:text-white  px-4 h-[40px] text-black font-medium/[24px] rounded-full">
                   <Link href="#">
